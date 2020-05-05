eb8a96b1eccd6c4ea0d6bf86193fe91ae0c5331e
Your Name <you@example.com>
2020-05-05 23:14:53 +0800

    使用ECharts处理专业技能

--- js/js.js
+++ js/js.js
@@ -1,4 +1,63 @@
-// 
+// echarts 表图 专业技能
+var myChart = echarts.init(document.getElementById('main'), 'westeros');
+
+option = {
+    title: {
+        text: '技能分布图',
+        left: 'center'
+    },
+    tooltip: {
+        trigger: 'item',
+        formatter: '{b} : {c} ({d}%)'
+    },
+    legend: {
+        left: 'center',
+        top: 'bottom',
+        data: ['HTML5', 'CSS3', 'JavaScript', 'JQuery', 'Bootstrap', 'Vue', 'git', 'other']
+    },
+   
+    series: [{
+        type: 'pie',
+        radius: [30, 110],
+        center: ['50%', '50%'],
+        roseType: 'area',
+        data: [{
+                value: 75,
+                name: 'HTML5'
+            },
+            {
+                value: 70,
+                name: 'CSS3'
+            },
+            {
+                value: 78,
+                name: 'JavaScript'
+            },
+            {
+                value: 70,
+                name: 'JQuery'
+            },
+            {
+                value: 70,
+                name: 'Bootstrap'
+            },
+            {
+                value: 75,
+                name: 'Vue'
+            },
+            {
+                value: 65,
+                name: 'git'
+            },
+            {
+                value: 70,
+                name: 'other'
+            }
+        ]
+    }]
+};
+myChart.setOption(option);
+// 主页轮播图
 var swiper1 = new Swiper('.swiper-container1', {
     direction: 'vertical',
     // followFinger: false,
@@ -33,31 +92,6 @@ function scrollFn(event) {
     }
 
 }
-// 技能
-$('.skills-content svg path').each(function (index, element) {
-    $(element).hover(function () {
-        // over
-        var ary = [
-            ['JavaScript', '65%'],
-            ['CSS', '65%'],
-            ['HTML', '65%'],
-            ['jQuery', '65%'],
-            ['bootstrap', '65%']
-        ]
-        $(this).attr('style', '-webkit-tap-highlight-color: rgba(0, 0, 0, 0); stroke-width: 50; opacity: 0.75;position: relative;z-index:2;');
-
-        $('.skills-content svg text').attr({
-            y: '295',
-            opacity: '0'
-        }).find('tspan').eq(0).text(ary[$(this).index() - 4][0]).next().text(ary[$(this).index() - 4][1]).parent().attr('opacity', '1');
-
-    }, function () {
-        // out
-        $(this).attr('style', '-webkit-tap-highlight-color: rgba(0, 0, 0, 0); stroke-width: 26; opacity: 1;');
-
-        $('.skills-content svg text').attr('y', '307').find('tspan').eq(0).text('Skills').next().text('');
-    });
-});
 
 // 项目轮播
 var swiper2 = new Swiper('.swiper-container2', {
@@ -73,22 +107,22 @@ var swiper2 = new Swiper('.swiper-container2', {
         clickable: true,
         // type : 'progressbar',
         type: 'fraction',
-      },
-    
+    },
+
 });
 
 // 项目轮播按钮
-$('.project-btn').each(function(index, element){
-    $(element).mousemove(function(e){
-        $(this).find('span').eq(0).attr('style','top:'+(e.offsetY-50)+'px;left:'+(e.offsetX-50)+'px')
+$('.project-btn').each(function (index, element) {
+    $(element).mousemove(function (e) {
+        $(this).find('span').eq(0).attr('style', 'top:' + (e.offsetY - 50) + 'px;left:' + (e.offsetX - 50) + 'px')
     })
-    if(index == 0){
-        $(element).click(function(){
+    if (index == 0) {
+        $(element).click(function () {
             swiper2.slidePrev();
         })
     }
-    if(index == 1){
-        $(element).click(function(){
+    if (index == 1) {
+        $(element).click(function () {
             swiper2.slideNext();
         })
     }
